e7665de0d2ca877fca05c953f6f36c9cd1126ac7
roger <liberatetheus@hotmail.com>
2020-08-25 16:59:20 +0800

    parallel hungarian method

--- src/lap_solvers/hungarian.py
+++ src/lap_solvers/hungarian.py
@@ -1,14 +1,15 @@
 import torch
 import scipy.optimize as opt
 import numpy as np
+from multiprocessing import Pool
 
-
-def hungarian(s: torch.Tensor, n1=None, n2=None):
+def hungarian(s: torch.Tensor, n1=None, n2=None, nproc=1):
     """
     Solve optimal LAP permutation by hungarian algorithm.
     :param s: input 3d tensor (first dimension represents batch)
     :param n1: [num of objs in dim1] (against padding)
     :param n2: [num of objs in dim2] (against padding)
+    :param nproc: number of parallel processes (default =1 for no parallel)
     :return: optimal permutation matrix
     """
     if len(s.shape) == 2:
@@ -23,15 +24,35 @@ def hungarian(s: torch.Tensor, n1=None, n2=None):
     batch_num = s.shape[0]
 
     perm_mat = s.cpu().detach().numpy() * -1
-    for b in range(batch_num):
-        n1b = perm_mat.shape[1] if n1 is None else n1[b]
-        n2b = perm_mat.shape[2] if n2 is None else n2[b]
-        row, col = opt.linear_sum_assignment(perm_mat[b, :n1b, :n2b])
-        perm_mat[b] = np.zeros_like(perm_mat[b])
-        perm_mat[b, row, col] = 1
+    if n1 is not None:
+        n1 = n1.cpu().numpy()
+    else:
+        n1 = [None] * batch_num
+    if n2 is not None:
+        n2 = n2.cpu().numpy()
+    else:
+        n2 = [None] * batch_num
+
+    if nproc > 1:
+        with Pool(processes=nproc) as pool:
+            mapresult = pool.starmap_async(hung_kernel, zip(perm_mat, n1, n2))
+            perm_mat = np.stack(mapresult.get())
+    else:
+        perm_mat = np.stack([hung_kernel(perm_mat[b], n1[b], n2[b]) for b in range(batch_num)])
+
     perm_mat = torch.from_numpy(perm_mat).to(device)
 
     if matrix_input:
         perm_mat.squeeze_(0)
 
+    return perm_mat
+
+def hung_kernel(s: torch.Tensor, n1=None, n2=None):
+    if n1 is None:
+        n1 = s.shape[0]
+    if n2 is None:
+        n2 = s.shape[1]
+    row, col = opt.linear_sum_assignment(s[:n1, :n2])
+    perm_mat = np.zeros_like(s)
+    perm_mat[row, col] = 1
     return perm_mat
