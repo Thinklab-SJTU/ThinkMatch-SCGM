1beddcd030f4aaab2384f76d3a8fad4ef5f76238
roger <liberatetheus@hotmail.com>
2021-01-12 19:21:18 +0800

    new PCA-GM model & experiment configs

--- models/PCA/model.py
+++ models/PCA/model.py
@@ -2,12 +2,13 @@ import torch
 import torch.nn as nn
 
 from src.lap_solvers.sinkhorn import Sinkhorn
-from models.GMN.displacement_layer import Displacement
 from src.feature_align import feature_align
 from src.gconv import Siamese_Gconv
 from models.PCA.affinity_layer import Affinity
+from src.lap_solvers.hungarian import hungarian
 
 from src.utils.config import cfg
+from models.PCA.model_config import model_cfg
 
 from src.backbone import *
 CNN = eval(cfg.BACKBONE)
@@ -16,8 +17,7 @@ CNN = eval(cfg.BACKBONE)
 class Net(CNN):
     def __init__(self):
         super(Net, self).__init__()
-        self.sinkhorn = Sinkhorn(max_iter=cfg.PCA.BS_ITER_NUM, epsilon=cfg.PCA.BS_EPSILON, tau=cfg.PCA.SK_TAU)
-        self.displacement_layer = Displacement()
+        self.sinkhorn = Sinkhorn(max_iter=cfg.PCA.SK_ITER_NUM, epsilon=cfg.PCA.SK_EPSILON, tau=cfg.PCA.SK_TAU)
         self.l2norm = nn.LocalResponseNorm(cfg.PCA.FEATURE_CHANNEL * 2, alpha=cfg.PCA.FEATURE_CHANNEL * 2, beta=0.5, k=0)
         self.gnn_layer = cfg.PCA.GNN_LAYER
         #self.pointer_net = PointerNet(cfg.PCA.GNN_FEAT, cfg.PCA.GNN_FEAT // 2, alpha=cfg.PCA.VOTING_ALPHA)
@@ -32,12 +32,20 @@ class Net(CNN):
                 self.add_module('cross_graph_{}'.format(i), nn.Linear(cfg.PCA.GNN_FEAT * 2, cfg.PCA.GNN_FEAT))
         self.cross_iter = cfg.PCA.CROSS_ITER
         self.cross_iter_num = cfg.PCA.CROSS_ITER_NUM
+        self.rescale = cfg.PROBLEM.RESCALE
 
     def reload_backbone(self):
         self.node_layers, self.edge_layers = self.get_backbone(True)
 
-    def forward(self, src, tgt, P_src, P_tgt, G_src, G_tgt, H_src, H_tgt, ns_src, ns_tgt, K_G, K_H, type='img'):
-        if type == 'img' or type == 'image':
+
+    def forward(self, data_dict, **kwargs):
+        if 'images' in data_dict:
+            # real image data
+            src, tgt = data_dict['images']
+            P_src, P_tgt = data_dict['Ps']
+            ns_src, ns_tgt = data_dict['ns']
+            A_src, A_tgt = data_dict['As']
+
             # extract feature
             src_node = self.node_layers(src)
             src_edge = self.edge_layers(src_node)
@@ -51,21 +59,22 @@ class Net(CNN):
             tgt_edge = self.l2norm(tgt_edge)
 
             # arrange features
-            U_src = feature_align(src_node, P_src, ns_src, cfg.PAIR.RESCALE)
-            F_src = feature_align(src_edge, P_src, ns_src, cfg.PAIR.RESCALE)
-            U_tgt = feature_align(tgt_node, P_tgt, ns_tgt, cfg.PAIR.RESCALE)
-            F_tgt = feature_align(tgt_edge, P_tgt, ns_tgt, cfg.PAIR.RESCALE)
-        elif type == 'feat' or type == 'feature':
+            U_src = feature_align(src_node, P_src, ns_src, self.rescale)
+            F_src = feature_align(src_edge, P_src, ns_src, self.rescale)
+            U_tgt = feature_align(tgt_node, P_tgt, ns_tgt, self.rescale)
+            F_tgt = feature_align(tgt_edge, P_tgt, ns_tgt, self.rescale)
+        elif 'features' in data_dict:
+            # synthetic data
+            src, tgt = data_dict['features']
+            ns_src, ns_tgt = data_dict['ns']
+            A_src, A_tgt = data_dict['As']
+
             U_src = src[:, :src.shape[1] // 2, :]
             F_src = src[:, src.shape[1] // 2:, :]
             U_tgt = tgt[:, :tgt.shape[1] // 2, :]
             F_tgt = tgt[:, tgt.shape[1] // 2:, :]
         else:
-            raise ValueError('unknown type string {}'.format(type))
-
-        # adjacency matrices
-        A_src = torch.bmm(G_src, H_src.transpose(1, 2))
-        A_tgt = torch.bmm(G_tgt, H_tgt.transpose(1, 2))
+            raise ValueError('Unknown data type for this model.')
 
         emb1, emb2 = torch.cat((U_src, F_src), dim=1).transpose(1, 2), torch.cat((U_tgt, F_tgt), dim=1).transpose(1, 2)
         ss = []
@@ -74,7 +83,7 @@ class Net(CNN):
             # Vanilla PCA-GM
             for i in range(self.gnn_layer):
                 gnn_layer = getattr(self, 'gnn_layer_{}'.format(i))
-                emb1, emb2 = gnn_layer([A_src, emb1], [A_tgt, emb2])
+                emb1, emb2 = gnn_layer([A_src, emb1], [A_tgt, emb2]) 
                 affinity = getattr(self, 'affinity_{}'.format(i))
                 s = affinity(emb1, emb2)
                 s = self.sinkhorn(s, ns_src, ns_tgt, dummy_row=True)
@@ -89,25 +98,29 @@ class Net(CNN):
                     emb2 = new_emb2
         else:
             # IPCA-GM
-            s = None
+            for i in range(self.gnn_layer - 1):
+                gnn_layer = getattr(self, 'gnn_layer_{}'.format(i))
+                emb1, emb2 = gnn_layer([A_src, emb1], [A_tgt, emb2])
+
+            emb1_0, emb2_0 = emb1, emb2
+            s = torch.zeros(emb1.shape[0], emb1.shape[1], emb2.shape[1], device=emb1.device)
+
             for x in range(self.cross_iter_num):
-                for i in range(self.gnn_layer):
-                    if i == 0:
-                        if s is None:
-                            gnn_layer = getattr(self, 'gnn_layer_{}'.format(i))
-                            emb1_0, emb2_0 = gnn_layer([A_src, emb1], [A_tgt, emb2])
-                            s = torch.zeros(emb1.shape[0], emb1.shape[1], emb2.shape[1], device=emb1.device)
+                i = self.gnn_layer - 2
+                cross_graph = getattr(self, 'cross_graph_{}'.format(i))
+                emb1 = cross_graph(torch.cat((emb1_0, torch.bmm(s, emb2_0)), dim=-1))
+                emb2 = cross_graph(torch.cat((emb2_0, torch.bmm(s.transpose(1, 2), emb1_0)), dim=-1))
 
-                        cross_graph = getattr(self, 'cross_graph_{}'.format(i))
-                        emb1 = cross_graph(torch.cat((emb1_0, torch.bmm(s, emb2_0)), dim=-1))
-                        emb2 = cross_graph(torch.cat((emb2_0, torch.bmm(s.transpose(1, 2), emb1_0)), dim=-1))
-                    else:
-                        gnn_layer = getattr(self, 'gnn_layer_{}'.format(i))
-                        emb1, emb2 = gnn_layer([A_src, emb1], [A_tgt, emb2])
-                        affinity = getattr(self, 'affinity_{}'.format(i))
-                        s = affinity(emb1, emb2)
-                        s = self.sinkhorn(s, ns_src, ns_tgt, dummy_row=True)
-                        ss.append(s)
+                i = self.gnn_layer - 1
+                gnn_layer = getattr(self, 'gnn_layer_{}'.format(i))
+                emb1, emb2 = gnn_layer([A_src, emb1], [A_tgt, emb2])
+                affinity = getattr(self, 'affinity_{}'.format(i))
+                s = affinity(emb1, emb2)
+                s = self.sinkhorn(s, ns_src, ns_tgt, dummy_row=True)
+                ss.append(s)
 
-        d, _ = self.displacement_layer(s, P_src, P_tgt)
-        return s, d
+        data_dict.update({
+            'ds_mat': ss[-1],
+            'perm_mat': hungarian(ss[-1], ns_src, ns_tgt)
+        })
+        return data_dict
--- models/PCA/model_config.py
+++ models/PCA/model_config.py
@@ -7,11 +7,10 @@ model_cfg = __C
 # PCA model options
 __C.PCA = edict()
 __C.PCA.FEATURE_CHANNEL = 512
-__C.PCA.BS_ITER_NUM = 20
-__C.PCA.BS_EPSILON = 1.0e-10
+__C.PCA.SK_ITER_NUM = 20
+__C.PCA.SK_EPSILON = 1.0e-10
 __C.PCA.SK_TAU = 0.005
 __C.PCA.GNN_LAYER = 5
 __C.PCA.GNN_FEAT = 1024
-__C.PCA.LOSS_WEIGHTS = [0., 1.]  # [cross-module loss, final prediction loss]
 __C.PCA.CROSS_ITER = False
 __C.PCA.CROSS_ITER_NUM = 1
