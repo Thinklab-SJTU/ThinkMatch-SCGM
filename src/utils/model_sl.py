f53341d227853c070b4b62cd0015cc7cb02d1a99
roger <liberatetheus@hotmail.com>
2021-03-12 22:43:06 +0800

    support non-strict model load

--- src/utils/model_sl.py
+++ src/utils/model_sl.py
@@ -9,8 +9,15 @@ def save_model(model, path):
     torch.save(model.state_dict(), path)
 
 
-def load_model(model, path):
+def load_model(model, path, strict=True):
     if isinstance(model, DataParallel):
-        model.module.load_state_dict(torch.load(path))
+        module = model.module
     else:
-        model.load_state_dict(torch.load(path))
+        module = model
+    missing_keys, unexpected_keys = module.load_state_dict(torch.load(path), strict=strict)
+    if len(unexpected_keys) > 0:
+        print('Warning: Unexpected key(s) in state_dict: {}. '.format(
+            ', '.join('"{}"'.format(k) for k in unexpected_keys)))
+    if len(missing_keys) > 0:
+        print('Warning: Missing key(s) in state_dict: {}. '.format(
+            ', '.join('"{}"'.format(k) for k in missing_keys)))
